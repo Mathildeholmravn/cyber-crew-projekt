441d4183374b2517e769ec402ef42d1c9fc197de
Mathildeholmravn <matrav01@iba.dk>
2026-05-03 10:02:59 +0700

    Rettelse af js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-const main = document.querySelector("main");
-let btns;
+const btns = document.querySelectorAll(".btn");
+const stages = document.querySelectorAll(".stage");
+const main = document.querySelector(".scenario-main"); /*forælder til .stage*/ 
 
 
-const updateUI = (pText, btnsText, imgPath, stageClass) => {
+
+const updateUI = (h3Text, pText, btnsText, imgPath) => {
 
 
 
@@ -11,32 +13,31 @@ const updateUI = (pText, btnsText, imgPath, stageClass) => {
     section.classList.add("stage");
     section.classList.add("active"); 
 
-    if (stageClass) {
-        section.classList.add(stageClass);
-    }
+    /* h3 */
+    const h3 = document.createElement("h3");
+    h3.textContent = h3Text;
+    section.append(h3);
 
-    if (imgPath !== undefined) {
+    /* Billede */
+    if(imgPath != undefined) {
         const img = document.createElement("img");
         img.src = imgPath;
-        img.alt = "billede af"; /* Skal rettes */
+        img.alt = "Stage billede";
         section.append(img);
     }
 
+    /* p */
     const p = document.createElement("p");
     p.textContent = pText;
-    section.append(p); 
+    section.append(p);
 
-    if (btnsText && btnsText.length > 0) {
-        btnsText.forEach(text => {
-            const button = document.createElement("button");
-            button.classList.add("btn");
-            button.textContent = text;
-            button.addEventListener("click", nextStage);
-            section.append(button);
-        })
-    }
-
-    btns = section.querySelectorAll(".btn");
+    /* Knapper */
+    btnsText.forEach(text => {
+        const button = document.createElement("button");
+        button.textContent = text;
+        button.addEventListener("click", nextStage);
+        section.append(button);
+    })
 
     main.replaceChildren(section); /* Slet alt i den gamle section og lav en ny section */
 }; 
@@ -45,26 +46,27 @@ const updateUI = (pText, btnsText, imgPath, stageClass) => {
 /* \n\n = linkeskift i tekst */
 
 const nextStage = (e) => {
-    let pText, btnsText, imgPath, stageClass;
+    let h3Text, pText, btnsText, imgPath;
 
     console.log("Knap klikket:", e.target.textContent);
-
     switch(e.target.textContent){
 
 /* STAGE 0 - Mistænkelig e-mail */
 case "START SCENARIE NU":
+    h3Text = "PHISING SCENARIE";
     pText = "Du har modtaget en mail fra Statens Uddannelsesstøtte. Læs den grundigt og træf et valg baseret på din viden omkring cybersikkerhed.";
     btnsText = [
     "Jeg reagerer hurtigt og klikker på linket.",
     "E-mailen virker mistænkelig - Jeg ignorerer den bare.",
     "Jeg sikrer mig at e-mailen ikke er falsk. Hellerer være på den sikre side!"]
 
-    imgPath = "img/placeholder.png";
+    imgPath = "img/forstørrelsesglasscenarie.png";
     break; 
 
 /* STAGE 1 - Klik på link */
 case "Jeg reagerer hurtigt og klikker på linket.":
-        pText = "Du klikker på linnket i mailen. Her bliver du bedt om at indtaste dine login-oplysninger. Indtaster du dine login-oplysninger?";
+        h3Text = "DU KLIKkER PÅ LINKET!";
+        pText = "Du klikker på linket i mailen. Her bliver du bedt om at indtaste dine login-oplysninger. Indtaster du dine login-oplysninger?";
         btnsText = [
             "Du indtaster de nødvendige oplysninger.",
             "Du bliver mistænksom og fortryder dit valg. Du lukker hurtigt browseren ned."
@@ -74,6 +76,7 @@ case "Jeg reagerer hurtigt og klikker på linket.":
 
 /* STAGE 2 - Tjek af detaljer */
 case "Jeg sikrer mig at e-mailen ikke er falsk. Hellere være på den sikre side!":
+        h3Text = "DETETKTIV MODE";
         pText = "Du tjekker om: Afsenderens e-mail adresse virker officiel og troværdig, sproget virker autogenereret eller mangelfuldt, virker mailen presserende og truende? og bliver du bedt om at videre give følsomme oplysninger?\n\nE-mailen opfylder alle ovenstående punkter og du konkluderer at e-mailen må være falsk. Hvad gør du nu?";
         btnsText = [
             "Jeg har ikke tænkt mig at åbne dette link!Jeg ignorerer mailen og kommer videre med min dag.",
@@ -85,6 +88,7 @@ case "Jeg sikrer mig at e-mailen ikke er falsk. Hellere være på den sikre side
 
 /* STAGE 3 - Mistænksomhed og fortydelse */
 case "Du bliver mistænksom og fortryder dit valg. Du lukker hurtigt browseren ned.":
+        h3Text = "HMM...DET VIRKER MISTÆNKELIGT!";
         pText = "Godt at du reagerede på din mavefornemmelse! Hackerne nåede heldigvis ikke at få fat i dine værdiefulde og personlige oplysninger. Hvad gør du nu?";
         btnsText = [
             "Jeg ignorerer mailen og kommer videre med min dag.",
@@ -95,45 +99,48 @@ case "Du bliver mistænksom og fortryder dit valg. Du lukker hurtigt browseren n
 
 /* Rød afslutning: De blev phisked! */
 case "Du indtaster de nødvendige oplysninger.":
+    h3Text = "PAS PÅ DINE OPLYSNINGER!";
     pText = "Du har indtastet dine oplysninger og hackerne kan nu logge ind på dine konto og misbruge dine personlige oplysninger.\n\nDu kan begrænse skaden ved at:\n• Skifte dine kodeord med det samme\n• Kontakte support og Statens Uddannelsesstøtte\n• Anmelde phising-angrebet\n\nSelv hvis du laver fejl, kan du stadig nå at reagere. Bliv en del af Cyber Crew. Sammen passer vi på hinanden."
-    btnsText = ["Start forfra"];
-    imgPath = "img/placeholder.png";
+    btnsText = ["START FORFRA"];
+    imgPath = "img/forstørrelsesglasscenarie.png";
     break;
 
 /* Orange afslutning: De blev phisked! */
 case "E-mailen virker mistænkelig - Jeg ignorerer den bare.":
 case "Jeg har ikke tænkt mig at åbne dette link!Jeg ignorerer mailen og kommer videre med min dag.":
 case "Jeg ignorerer mailen og kommer videre med min dag.":
+    h3Text = "DU ER SIKKER!";
     pText = "Du er stadig sikker! Men dine medstuderende er måske ikke.\n\nHer er nogle tips:\n• Husk at være skeptisk over for mails der presser dig\n• Check altid afsenderens e-mail adresse\n• Se efter stavefejl og autogenereret sprog\n\nBliv en del af Cyber Crew. Sammen passer vi på hinanden.";
-    btnsText = ["Start forfra"];
+    btnsText = ["START FORFRA"];
     imgPath = "img/håndeklapper-ikon.png";
     break;
 
 
 /* Grøn afslutning: De blev phisked! */
 case "Forbandet svindlere! Jeg må sørge for at anmelde e-mailen!":
+    h3Text = "DU ER SIKKER! GODT KLARET";
     pText = "Ved at anmelde e-mailen blev phising-kampangen stoppet og du hjalp med at beskytte dine medstuderende!\n\nDu har nu en vigtig rolle i at sikre vores fælles cyber-fremtid.\n\nBliv en del af Cyber Crew. Vi hjælper hinanden til en sikker cyber fremtid.";
-    btnsText = ["Start forfra"];
+    btnsText = ["START FORFRA"];
     imgPath = "img/håndeklapper-ikon.png";
     break;
 
 /* Start forfra */
-case "Start forfra":
+case "START FORFRA":
+    h3Text = "PHISHING SCENARIE";
     pText = "Du har modtaget en mail fra Statens Uddannelsesstøtte. Læs den grundigt og træf et valg baseret på din viden omkring cybersikkerhed.";
     btnsText = [
     "Jeg reagerer hurtigt og klikker på linket.",
     "E-mailen virker mistænkelig - Jeg ignorerer den bare.",
     "Jeg sikrer mig at e-mailen ikke er falsk. Hellerer være på den sikre side!"]
 
-    imgPath = "img/placeholder.png";
+    imgPath = "img/forstørrelsesglasscenarie.png";
     break; 
 
     }   
-     updateUI(pText, btnsText, imgPath, stageClass);
+     updateUI(h3Text, pText, btnsText, imgPath);
 }       
     
 
-/* Event listeners - lyt efter klik på knapper */
 
 
 
